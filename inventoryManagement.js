50a626fc3488c9290e0d817f261c993c00bc2912
piperpei <piperpei@yahoo.com>
2025-07-25 11:53:45 -0700

    Array Lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,32 @@
 // Write your code here
 
+// Product Array 
 
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+// Function to Grab First Product Info 
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+// Function to Add a Product 
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+// Add New Product and Name 
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+// Funtion that Removes Last Product 
+
+function removeLastProduct() {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
